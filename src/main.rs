824619cf8a191e68287b12ad43bdcf4628f466e8
Karmpatel18 <karmpatel1203@gmail.com>
2025-03-14 01:03:32 +0530

    mutable

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ fn main() {
     // print!("\n");
     // let a = do_sum(10, 20);
     // println!("{}", a);
+
+    let mut a: i32 = 10;
+    let a=12;  //a is immutable here we cannot change the value of a . to change it make it mutable by adding mut keyword before the variable 
+    println!("{}", a);
+
+
 }
 
 // fn do_sum(a: i32, b: i32) -> i32 {
