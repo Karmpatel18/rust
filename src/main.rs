684574ca1d15a8ccb3b51e93ee4e20ef52557bf6
Karmpatel18 <karmpatel1203@gmail.com>
2025-03-14 23:38:24 +0530

    stack vs heap

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
 fn main() {
-    // for i in 0..10 {
-    //     print!("{}", i);
-    // }
-    // print!("\n");
-    // let a = do_sum(10, 20);
-    // println!("{}", a);
+    
 
-    let mut a: i32 = 10;
-    let a=12;  //a is immutable here we cannot change the value of a . to change it make it mutable by adding mut keyword before the variable 
-    println!("{}", a);
+    stack_fn();
+    heap_fn();
+    stringupdate_fn();
+
 
 
 }
 
-// fn do_sum(a: i32, b: i32) -> i32 {
-//     return a+b;
-// }
+fn stack_fn(){
+    let x = 10;
+    let y = 12;
+    let z = x + y;
+    println!("{}",z);
+}
+fn heap_fn(){
+    let s1 = String::from("hello");
+    let s2 = String::from("world");
+    // println!("{} {}",s1,s2);
+    let z = format!("{} {}",s1,s2);
+    println!("{}",z);
+}
+
+fn stringupdate_fn(){
+    let mut string = String::from("hello world");
+    println!("{}",string);
+    string.push_str("after updatation");
+}
+
