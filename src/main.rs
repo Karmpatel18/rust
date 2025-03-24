6e87b1cef45ed5258802649af49bb90e476d8c38
Karmpatel18 <karmpatel1203@gmail.com>
2025-03-25 00:05:57 +0530

    enums in rust

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
+// struct User {
+//     username: String,
+//     email: String,
+//     sign_in_count: u64,
+//     active: bool,
+// }
+// fn main(){
+//     let user = User {
+//         username: String::from("someusername123"),
+//         email: String::from("s9xHx@example.com"),   
+//         sign_in_count: 1,
+//         active: true
+//     };
+//     println!("{}",user.username);
+// }
+
+enum Direction {
+    North,
+    South,
+    East,
+    West
 }
+
 fn main(){
-    let user = User {
-        username: String::from("someusername123"),
-        email: String::from("s9xHx@example.com"),   
-        sign_in_count: 1,
-        active: true
-    };
-    println!("{}",user.username);
+    let newdirection = Direction::North;
+    moveplayer(newdirection);
+    
 }
+    
+fn moveplayer(direction:Direction){
+        println!("hello ffrom mvoeplayer function",);
+}
+        
+    
