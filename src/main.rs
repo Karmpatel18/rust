5ac98f02fb08ec308399c9978aeca8fe04c1cd86
Karmpatel18 <karmpatel1203@gmail.com>
2025-03-25 00:20:28 +0530

    enums with values

--- src/main.rs
+++ src/main.rs
@@ -14,21 +14,35 @@
 //     println!("{}",user.username);
 // }
 
-enum Direction {
-    North,
-    South,
-    East,
-    West
+enum Shape {
+    Rectangle(f64,f64),
+    Circle(f64),
 }
 
-fn main(){
-    let newdirection = Direction::North;
-    moveplayer(newdirection);
+// fn main(){
+//     let newdirection = Direction::North;
+//     moveplayer(newdirection);
     
-}
+// }
     
-fn moveplayer(direction:Direction){
-        println!("hello ffrom mvoeplayer function",);
-}
-        
+// fn moveplayer(direction:Direction){
+//         println!("hello ffrom mvoeplayer function",);
+// }
+
+
+    fn main(){
+        let rect = Shape::Rectangle(1.0,2.0);
+        let circle = Shape::Circle(1.0);
+    let result =    calulate_area(circle);
+        println!("area of circle is {}",result);
+    }
+
+    fn calulate_area(shape:Shape) -> f64{
+        let area = match shape {
+            Shape::Rectangle(a,b) => a*b ,
+            Shape::Circle(r)=>3.14*r*r,
+            
+        };
+        return area;
+    }
     
